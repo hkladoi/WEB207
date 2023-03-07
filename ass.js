c03bf0cf088678ca87d035ebdf3a961c97302cc4
hkladoi <anhtrinhtran3@gmail.com>
2023-03-08 02:28:05 +0700

    code

--- ass.js
+++ ass.js
@@ -1,39 +1,50 @@
-window.ShowSP = function ($scope, $http, $location, $rootScope) {
-    $scope.lstSP = [];
+function showHideOptions() {
+    var category = document.getElementById("loai");
+    var sizeOptions = document.getElementById("size");
+    var mauOptions = document.getElementById("mau");
+    if (category.value === "Đồ lưu niệm Mixi") {
+        mauOptions.setAttribute("hidden", "");
+        sizeOptions.setAttribute("hidden", "");
+    } else {
+        sizeOptions.removeAttribute("hidden");
+        mauOptions.removeAttribute("hidden");
+    }
+}
+window.ShowSP = function ($scope, $http, $location, $routeParams) {
+    $scope.lst = [];
     $http.get("http://localhost:3000/sanpham").then(function (response) {
-        $scope.lstSP = response.data;
+        $scope.lst = response.data;
     },
         function (error) {
             console.log("Error: " + error);
         }
     );
 
-        $scope.trangthai = "NEW";
-        $scope.add = function () {
-        if ($scope.name == null || $scope.price == null || $scope.color == null || $scope.Size == null || $scope.type == null || isNaN($scope.price) || isNaN($scope.sale)) {
+    $scope.trangthai = "NEW";
+    $scope.add = function () {
+        if ($scope.ten == null || $scope.giaban == null || $scope.mausac == null || $scope.size == null || $scope.theloai == null || isNaN($scope.giaban) || isNaN($scope.giamgia)) {
             alert("Vui lòng nhập đầy đủ thông tin và nhập đúng thông tin!");
-            window.location.reload('/quantri');
+            window.location.reload();
         }
-        else if ($scope.name.trim() === '' || $scope.color.trim() === '' || $scope.price.trim() === '') {
+        else if ($scope.ten.trim() === '' || $scope.mausac.trim() === '' || $scope.giaban.trim() === '') {
             alert("Không được nhập nhiều khoảng trắng như vậy");
-            window.location.reload('/quantri');
+            window.location.reload();
         }
         else {
-            $scope.id=null;
+            $scope.id = null;
             $http.post("http://localhost:3000/sanpham", {
                 id: $scope.id,
-                name: $scope.name,
-                price: $scope.price,
-                type: $scope.type,
-                color: $scope.color,
-                Size: $scope.Size,
-                Image: $scope.Image,
-                sale: $scope.sale,
+                ten: $scope.ten,
+                giaban: $scope.giaban,
+                theloai: $scope.theloai,
+                mausac: $scope.mausac,
+                size: $scope.size,
+                giamgia: $scope.giamgia,
                 trangthai: $scope.trangthai
             }).then(function (response) {
                 if (response.status === 201) {
                     alert("Thêm thành công ");
-                    window.location.reload('/quantri');
+                    window.location.reload();
                 }
             },
                 function (error) {
@@ -43,55 +54,71 @@ window.ShowSP = function ($scope, $http, $location, $rootScope) {
         }
 
     };
-    $scope.chitiet = function (id) {
+    
+    $scope.detail = function (id) {
         $http.get("http://localhost:3000/sanpham/" + id).then(function (response) {
-            if (response.status === 200) {
+            var sizeOptions = document.getElementById("size");
+            var mauOptions = document.getElementById("mau");
+            if (response.data.theloai === "Đồ lưu niệm Mixi") {
                 $scope.id = response.data.id;
-                $scope.name = response.data.name;
-                $scope.price = response.data.price;
-                $scope.type = response.data.type;
-                $scope.color = response.data.color;
-                $scope.Size = response.data.Size;
-                $scope.Image = response.data.Image;
-                $scope.sale = response.data.sale;
+                $scope.ten = response.data.ten;
+                $scope.giaban = response.data.giaban;
+                $scope.theloai = response.data.theloai;
+                $scope.mausac = response.data.mausac;
+                $scope.size = response.data.size;
+                $scope.giamgia = response.data.giamgia;
                 $scope.trangthai = response.data.trangthai;
+                sizeOptions.setAttribute("hidden","");
+                mauOptions.setAttribute("hidden","");
+            } else {
+                $scope.id = response.data.id;
+                $scope.ten = response.data.ten;
+                $scope.giaban = response.data.giaban;
+                $scope.theloai = response.data.theloai;
+                $scope.mausac = response.data.mausac;
+                $scope.size = response.data.size;
+                $scope.giamgia = response.data.giamgia;
+                $scope.trangthai = response.data.trangthai;
+                sizeOptions.removeAttribute("hidden");
+                mauOptions.removeAttribute("hidden");
             }
+
         },
             function (error) {
                 console.log("Error: " + error);
             }
         );
-    };
+    }
+
     $scope.update = function () {
         if ($scope.id == null) {
             alert("Vui lòng chọn sản phẩm cần sửa");
-            window.location.reload('/quantri');
+            window.location.reload();
         }
-        else if ($scope.name == null || $scope.price == null || $scope.Size == null || $scope.color == null || $scope.type == null || isNaN($scope.price) || isNaN($scope.sale)) {
+        else if ($scope.ten == null || $scope.giaban == null || $scope.size == null || $scope.mausac == null || $scope.theloai == null || isNaN($scope.giaban) || isNaN($scope.giamgia)) {
             alert("Vui lòng nhập đầy đủ thông tin và nhập đúng thông tin!");
-            window.location.reload('/quantri');
+            window.location.reload();
         }
-        else if ($scope.name.trim() === '' || $scope.color.trim() === '' || $scope.price.trim() === '') {
+        else if ($scope.ten.trim() === '' || $scope.mausac.trim() === '' || $scope.giaban.trim() === '') {
             alert("Không được nhập nhiều khoảng trắng như vậy");
-            window.location.reload('/quantri');
+            window.location.reload();
         }
         else {
             $http.put("http://localhost:3000/sanpham/" + $scope.id,
                 {
                     id: $scope.id,
-                    name: $scope.name,
-                    price: $scope.price,
-                    type: $scope.type,
-                    color: $scope.color,
-                    Image: $scope.Image,
-                    Size: $scope.Size,
-                    sale: $scope.sale,
+                    ten: $scope.ten,
+                    giaban: $scope.giaban,
+                    theloai: $scope.theloai,
+                    mausac: $scope.mausac,
+                    size: $scope.size,
+                    giamgia: $scope.giamgia,
                     trangthai: $scope.trangthai
                 }
             ).then(function (response) {
                 if (response.status == 200) {
                     alert("Cập nhật thành công ");
-                    window.location.reload('/quantri');
+                    window.location.reload();
                 }
 
             });
@@ -103,18 +130,17 @@ window.ShowSP = function ($scope, $http, $location, $rootScope) {
         $http.delete("http://localhost:3000/sanpham/" + id).then(function (response) {
             if (response.status == 200) {
                 alert("xóa thành công");
-                window.location.reload('/quantri');
+                window.location.reload();
             }
         },
             function (error) {
                 console.log("Error: " + error);
             });
     };
-    $rootScope.product = {};
-    $scope.getSanPham = function (id) {
-        $http.get("http://localhost:3000/sanpham/" + id).then(function (response) {
-            $rootScope.product = response.data;
-        })
-    }
-    
+
+    var id = $routeParams.id;
+    $http.get("http://localhost:3000/sanpham/" + id).then(function(response) {
+      $scope.ctsp = response.data;
+    });
 }
+
